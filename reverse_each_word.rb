a075c065b100d430f58d3bc0597b5f757dbf5d77
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-15 15:11:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
+# def reverse_each_word(str)
+#   words = str.split(" ")
+#   reversed = []
+#   words.each do |word|
+#     reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
+
 def reverse_each_word(str)
   words = str.split(" ")
   reversed = []
